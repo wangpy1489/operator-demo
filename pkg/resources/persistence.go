a76857a7961c1f4a7823b9451c47d152b0aeeb21
Cloud User <wangpy1489@163.com>
2019-11-28 10:38:53 +0000

    fix build bug

--- pkg/resources/persistence.go
+++ pkg/resources/persistence.go
@@ -1,7 +1,7 @@
 package resources
 
 import (
-	appsv1 "k8s.io/api/apps/v1"
+	// appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
